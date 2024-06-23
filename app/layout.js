880134aa88d6f311ddab9696bfb72cc3c930da7b
jem-5 <jaelyazi@gmail.com>
2024-06-23 14:18:28 -0400

    add manual script for analytics

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Image from "next/image";
 import BgImg from "../public/bg.jpg";
 import BgMobileImg from "../public/bg-mobile.jpg";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata = {
   title: "Learn Arabic Today",
@@ -16,8 +17,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="w-screen h-full ">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q9R3EYD27Y"
+        />
+
+        <Script id="google-analytics">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${"${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"});
+  `}
+        </Script>
+      </head>
+
       {/* <GoogleTagManager gtmId /> */}
-      <GoogleAnalytics gaId="G-Q9R3EYD27Y" />
+      {/* <GoogleAnalytics gaId="G-Q9R3EYD27Y" /> */}
       <body className=" min-h-full flex flex-col justify-between items-center  min-w-screen   bg-[url('/bg-mobile.jpg')]  md:bg-[url('/bg.jpg')] bg-cover">
         <AuthContextProvider>
           <Navigation />
