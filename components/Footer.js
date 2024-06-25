3ef8a0499b934b12212ef5198eab6b9bddb747b5
jem-5 <jaelyazi@gmail.com>
2024-06-25 09:47:00 -0400

    add privacy policy to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <footer className="footer footer-center p-2 bg-base-300 text-base-content">
       <aside>
-        <p>Copyright © 2024 - All rights reserved</p>
+        <p>
+          Copyright © 2024 - All rights reserved |{" "}
+          <a href="/privacy">Privacy Policy</a>
+        </p>
       </aside>
     </footer>
   );
