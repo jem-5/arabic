47c3729aaad7dcdc408981de5759289696280321
jem-5 <jaelyazi@gmail.com>
2024-06-26 11:12:42 -0400

    add g tag

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', ${"${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"});
+      gtag('config', 'G-Q9R3EYD27Y');
   `}
         </Script>
       </head>
