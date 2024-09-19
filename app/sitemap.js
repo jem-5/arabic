675f0b5b52eca8a8e1479bbe527ae099f444c2ec
jem-5 <jaelyazi@gmail.com>
2024-09-19 14:39:24 -0400

    fix sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,58 +1,88 @@
-export default function sitemap() {
-  return [
-    {
-      url: "https://arabicroad.com/",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: "https://arabicroad.com/dashboard/",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://arabicroad.com/lesson/",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://arabicroad.com/quiz/",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://arabicroad.com/blog/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://arabicroad.com/about/",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-    {
-      url: "https://arabicroad.com/contact/",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-    {
-      url: "https://arabicroad.com/privacy/",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-    {
-      url: "https://arabicroad.com/terms/",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-  ];
+// app/sitemap.js
+
+import { getSortedPostsData } from "../_posts";
+
+const URL = "https://arabicroad.com";
+
+export default async function sitemap() {
+  const posts = getSortedPostsData.map(({ id, date }) => ({
+    url: `${URL}/blog/${id}`,
+    lastModified: date,
+  }));
+
+  const routes = [
+    "",
+    "/dashboard",
+    "/lesson",
+    "/quiz",
+    "/blog",
+    "/about",
+    "/contact",
+    "/privacy",
+    "/terms",
+  ].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routes, ...posts];
 }
+
+// export default function sitemap() {
+//   return [
+//     {
+//       url: "https://arabicroad.com/",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 1,
+//     },
+//     {
+//       url: "https://arabicroad.com/dashboard/",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.8,
+//     },
+//     {
+//       url: "https://arabicroad.com/lesson/",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.8,
+//     },
+//     {
+//       url: "https://arabicroad.com/quiz/",
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.8,
+//     },
+//     {
+//       url: "https://arabicroad.com/blog/",
+//       lastModified: new Date(),
+//       changeFrequency: "weekly",
+//       priority: 0.5,
+//     },
+//     {
+//       url: "https://arabicroad.com/about/",
+//       lastModified: new Date(),
+//       changeFrequency: "yearly",
+//       priority: 0.3,
+//     },
+//     {
+//       url: "https://arabicroad.com/contact/",
+//       lastModified: new Date(),
+//       changeFrequency: "yearly",
+//       priority: 0.3,
+//     },
+//     {
+//       url: "https://arabicroad.com/privacy/",
+//       lastModified: new Date(),
+//       changeFrequency: "yearly",
+//       priority: 0.3,
+//     },
+//     {
+//       url: "https://arabicroad.com/terms/",
+//       lastModified: new Date(),
+//       changeFrequency: "yearly",
+//       priority: 0.3,
+//     },
+//   ];
+// }
