c63d6c7dcbaea7b3a4464acc722a1457a61db398
jem-5 <jaelyazi@gmail.com>
2024-09-20 11:47:46 -0400

    add metadata

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,12 @@ import Footer from "@/components/Footer";
 
 import Script from "next/script";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#ffffff",
+};
+
 export const metadata = {
   title:
     "Arabic Road - Learn Arabic Online | Free Arabic Language Learning Platform",
@@ -12,9 +18,47 @@ export const metadata = {
     "Learn Arabic for free with ArabicRoad! Access vocabulary with relevant photos, Arabic voice narration, and English transliteration. No account needed. Start learning today!",
   keywords:
     "Learn Arabic, Arabic language, Arabic lessons, Free Arabic learning, Arabic vocabulary, Arabic pronunciation, Arabic voice narration, Arabic transliteration, Arabic for beginners, ArabicRoad",
-  robots: "index, follow",
-  viewport: "width=device-width, initial-scale=1.0",
   charset: "UTF-8",
+  metadataBase: new URL("https://arabicroad.com"),
+  openGraph: {
+    siteName:
+      "Arabic Road - Learn Arabic Online | Free Arabic Language Learning Platform",
+    type: "website",
+    locale: "en_US",
+
+    url: "https://arabicroad.com/",
+    title:
+      "Arabic Road - Learn Arabic Online | Free Arabic Language Learning Platform",
+    description:
+      "Learn Arabic for free with ArabicRoad! Access vocabulary with relevant photos, Arabic voice narration, and English transliteration. No account needed. Start learning today!",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+    "max-video-preview": -1,
+    googleBot: "index, follow",
+  },
+  applicationName:
+    "Arabic Road - Learn Arabic Online | Free Arabic Language Learning Platform",
+  appleWebApp: {
+    title:
+      "Arabic Road - Learn Arabic Online | Free Arabic Language Learning Platform",
+    statusBarStyle: "default",
+    capable: true,
+  },
+  icons: {
+    icon: [
+      {
+        url: "/icon.ico",
+        type: "image/x-icon",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "https://arabicroad.com/",
+  },
 };
 
 export default function RootLayout({ children }) {
