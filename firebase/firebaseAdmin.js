1d4ea4e8963236e6458ec6ccf4cecf98587a56ef
jem-5 <jaelyazi@gmail.com>
2025-06-25 11:59:18 -0400

    fix process env

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -2,9 +2,11 @@ import admin from "firebase-admin";
 import { readFileSync } from "fs";
 import { join } from "path";
 
-const serviceAccount = JSON.parse(
-  readFileSync(join(process.cwd(), "serviceAccountKey.json"), "utf8")
-);
+// const serviceAccount = JSON.parse(
+//   readFileSync(join(process.cwd(), "serviceAccountKey.json"), "utf8")
+// );
+
+const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_SERVICE_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
