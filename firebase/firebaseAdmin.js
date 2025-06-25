79898ecac84d70952a3e8d3f97d5d5a19143a7dc
jem-5 <jaelyazi@gmail.com>
2025-06-25 12:08:12 -0400

    add service key

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,12 +1,12 @@
 import admin from "firebase-admin";
 import { readFileSync } from "fs";
 import { join } from "path";
-
+import serviceAccount from "/etc/secrets/serviceAccountKey.json";
 // const serviceAccount = JSON.parse(
 //   readFileSync(join(process.cwd(), "serviceAccountKey.json"), "utf8")
 // );
 
-const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_SERVICE_KEY);
+// const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_SERVICE_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
